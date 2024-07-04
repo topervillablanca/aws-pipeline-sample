03d7110096641057521ca975a75bc59d5e538b6d
topervillablanca <topervillablanca@gmail.com>
2024-07-04 18:58:25 +0800

    lambda updates

--- sample-project-pipeline/lib/sample-project-pipeline-stack.ts
+++ sample-project-pipeline/lib/sample-project-pipeline-stack.ts
@@ -1,24 +1,48 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ManualApprovalStep, ShellStep } from 'aws-cdk-lib/pipelines';
+import { PipelineAppStage } from './stage';
+import { ManualApprovalAction } from 'aws-cdk-lib/aws-codepipeline-actions';
 
 export class SampleProjectPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: "testPipeline",
       synth: new ShellStep('synth', {
         input: CodePipelineSource.gitHub('topervillablanca/aws-pipeline-sample', 'master'),
         commands: [
-          'cd sample-project-pipeline-stack', 
           'npm ci',
           'npm run build',
-          'npm cdk synth'
+          'npx cdk synth'
         ],
-        primaryOutputDirectory: 'sample-project-pipeline-stack/cdk.out',
       })
     })
+
+    const devStage = pipeline.addStage(new PipelineAppStage(this, "DEV", {
+      env: {
+        account: "992382358172",
+        region: "us-east-1"
+      }
+    }))
+
+    const qaStage = pipeline.addStage(new PipelineAppStage(this, "QA", {
+      env: {
+        account: "992382358172",
+        region: "us-east-1"
+      }
+    }))
+
+    qaStage.addPost(new ManualApprovalStep('Needs approval from the Dev team before releasing to QA'))
+
+    const prodStage = pipeline.addStage(new PipelineAppStage(this, "PROD", {
+      env: {
+        account: "992382358172",
+        region: "us-east-1"
+      }
+    }))
+
+    prodStage.addPost(new ManualApprovalStep('Needs approval from the Dev and QA team before releasing to PROD'))
   }
 }
